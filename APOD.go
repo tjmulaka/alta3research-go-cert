025d9928c44459df47b10a9c277ff2806325c7c6
tjmulaka <95987177+tjmulaka@users.noreply.github.com>
2021-12-11 18:38:16 -0500

    Add files via upload

---
+++ APOD.go
@@ -0,0 +1,15 @@
+/*
+ APOD (Astronomy Picture of the Day.) attributes/ properties.
+*/
+package model
+
+type APOD struct {
+	Copyright      string `json:"copyright"`
+	Date           string `json:"date"`
+	Explanation    string `json:"explanation"`
+	Hdurl          string `json:"hdurl"`
+	MediaType      string `json:"media_type"`
+	ServiceVersion string `json:"service_version"`
+	Title          string `json:"title"`
+	URL            string `json:"url"`
+}
